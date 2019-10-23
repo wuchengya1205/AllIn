99fa463564af29238748ee2f73115f028bad04e1
fengzhangwei <fengzhangw@foxmail.com>
2019-10-23 17:53:08 +0800

    欢迎页

--- app/src/main/java/com/xiang/allin/FirstPage/WelcomeActivity.java
+++ app/src/main/java/com/xiang/allin/FirstPage/WelcomeActivity.java
@@ -12,32 +12,30 @@ import com.xiang.allin.R;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
 
-    int time = 3;
+    int time = 4;
     private TextView texttime;
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         initView();
-        texttime.setText("倒计时:"+time);
-        final Timer timer = new Timer();
+        texttime.setText(String.valueOf(time));
+        timer = new Timer();
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if (time > 0) {
+                        if (time > 1) {
                             time--;
-                            texttime.setText("倒计时:"+time);
+                            texttime.setText(String.valueOf(time));
                         } else {
-                            timer.cancel();
-                            startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
-                            texttime.setVisibility(View.GONE);
-                            finish();
+                            goActivity();
                         }
                     }
                 });
@@ -47,7 +45,22 @@ public class WelcomeActivity extends AppCompatActivity {
         timer.schedule(timerTask, 0, 1000);
     }
 
+    private void goActivity() {
+        timer.cancel();
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+        texttime.setVisibility(View.VISIBLE);
+        finish();
+    }
+
     private void initView() {
         texttime = (TextView) findViewById(R.id.texttime);
+        texttime.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.texttime){
+            goActivity();
+        }
     }
 }
