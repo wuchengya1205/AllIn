6f7ccd4641f71db4fee634ca73a3e1c394618726
wucy <wucy1205@yeah.net>
2019-10-25 09:49:25 +0800

    wucy

---
+++ app/src/main/java/com/xiang/allin/FirstPage/FirstPageContract.java
@@ -0,0 +1,37 @@
+package com.xiang.allin.FirstPage;
+
+import com.xiang.allin.common.CommonBean;
+
+import java.util.List;
+import java.util.Map;
+
+import mvp.ljb.kt.contract.IPresenterContract;
+import mvp.ljb.kt.contract.IViewContract;
+
+/**
+ * author : wuchengya
+ * e-mail : wucy1205@yeah.net
+ * date   : 2019/10/22
+ * time   :11:32
+ * desc   :ohuo
+ * version: 1.0
+ */
+public interface FirstPageContract {
+    interface IView extends IViewContract {
+
+        void getDataSuccess(CommonBean commonBean);
+
+        void getDataError(String msg);
+
+        Map<String,String> getParams();
+
+        String getType();
+
+        String getKey();
+    }
+
+    interface IPresenter extends IPresenterContract {
+
+        void getData();
+    }
+}
